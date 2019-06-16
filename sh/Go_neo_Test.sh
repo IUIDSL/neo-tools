68b0112581007aefd18c6ebf269bcff15915ecd6
Jeremy Yang <jeremyjyang@gmail.com>
2019-06-16 10:04:41 -0600

    Ok.

--- sh/Go_neo_Test.sh
+++ sh/Go_neo_Test.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
-#
+#############################################################################
+### See: https://neo4j-client.net/
+#############################################################################
 DBHOST="cheminfov.informatics.indiana.edu"
 DBUSR=""
 DBPW=""
 #
-neo4j-client -u "$DBUSR" -p "$DBPW" \
-	-o data/example_01_out.txt \
-	-i cql/example_01.cql \
+cwd=$(pwd)
+#
+if [ "$(which cypher-shell)" ]; then
+	CQLAPP="cypher-shell"
+elif [ "$(which neo4j-client)" ]; then
+	CQLAPP="neo4j-client"
+else
+	echo "ERROR: Neo4j/CQL client app not found."
+	exit
+fi
+printf "CQLAPP = %s\n" "$CQLAPP"
+#
+#
+$CQLAPP -u "$DBUSR" -p "$DBPW" \
+	-o ${cwd}/data/example_01_out.txt \
+	-i ${cwd}/cql/example_01.cql \
 	"$DBHOST"
 #
