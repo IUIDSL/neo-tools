62bb177ef2196b3b59899d20df485d6b610d9105
Jeremy J Yang <jeremyjyang@gmail.com>
2019-06-16 10:32:09 -0600

    Ok.

--- sh/Go_neo_Test.sh
+++ sh/Go_neo_Test.sh
@@ -1,26 +1,36 @@
 #!/bin/bash
 #############################################################################
+### See: https://neo4j.com/docs/operations-manual/current/tools/cypher-shell/
 ### See: https://neo4j-client.net/
 #############################################################################
+#
+if [ $# -lt 1 ]; then
+	printf "ERROR: Syntax: %s CQLFILE\n" $(basename $0)
+else
+	CQLFILE=$1
+fi
+#
 DBHOST="cheminfov.informatics.indiana.edu"
-DBUSR=""
-DBPW=""
 #
 cwd=$(pwd)
 #
 if [ "$(which cypher-shell)" ]; then
 	CQLAPP="cypher-shell"
+	printf "CQLAPP = %s\n" "$CQLAPP"
+	#
+	$CQLAPP -a "bolt://${DBHOST}:7687" \
+		--non-interactive \
+		--format "plain" \
+		<${CQLFILE}
+	#
 elif [ "$(which neo4j-client)" ]; then
 	CQLAPP="neo4j-client"
+	printf "CQLAPP = %s\n" "$CQLAPP"
+	#
+	$CQLAPP -i ${CQLFILE} "$DBHOST"
+	#
 else
 	echo "ERROR: Neo4j/CQL client app not found."
 	exit
 fi
-printf "CQLAPP = %s\n" "$CQLAPP"
-#
-#
-$CQLAPP -u "$DBUSR" -p "$DBPW" \
-	-o ${cwd}/data/example_01_out.txt \
-	-i ${cwd}/cql/example_01.cql \
-	"$DBHOST"
 #
